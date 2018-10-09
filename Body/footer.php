ea9fb7d04b0a29398f3a9aacc6f27ac828d60c7e
David Mendez Guardado <demg@outlook.com>
2018-10-09 11:16:55 -0500

    -Change bootstrap theme. -Delete files not nedded. -Use PHP for all the project.

---
+++ Body/footer.php
@@ -0,0 +1,16 @@
+<hr />
+<footer>
+    <div class="row">
+        <div class="col-lg-6 workshop-left text-muted">
+            <p>
+                &copy;
+                <strong>
+                    <?php echo date("Y"); ?>
+                </strong> - LSCA David Mendez Guardado
+            </p>
+        </div>
+    </div>
+</footer>
+
+<script src="./js/jquery-3.3.1.js"></script>
+<script src="./js/bootstrap.js"></script>
